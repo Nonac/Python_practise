1bbd8ebd74b0ecdc3a0c3d11d0e49c43be400a79
Nonac <NonacYang@gmail.com>
2018-02-24 14:02:21 +0800

    first upload
    test

---
+++ Monkey_Typing.py
@@ -0,0 +1,15 @@
+def count_words(text, words):
+    n = 0
+    j = text.lower()
+    for i in words:
+        if j.count(i) > 0:n = n + 1
+    return n
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert count_words("How aresjfhdskfhskd you?", {"how", "are", "you", "hello"}) == 3, "Example"
+    assert count_words("Bananas, give me bananas!!!", {"banana", "bananas"}) == 2, "BANANAS!"
+    assert count_words("Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+                       {"sum", "hamlet", "infinity", "anything"}) == 1, "Weird text"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
